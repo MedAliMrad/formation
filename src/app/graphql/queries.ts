b16bd7f38203685bb05496716b8f60a529b74a3a
MedAliMrad <mohamedalimrad03@gmail.com>
2025-07-29 11:12:41 +0100

    integration of FrontEnd and Backend 1

--- src/app/graphql/queries.ts
+++ src/app/graphql/queries.ts
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+
 export const SEARCH_CLIENTS = gql`
   query SearchClients($searchTerm: String!) {
     searchClients(searchTerm: $searchTerm) {
@@ -8,5 +9,28 @@ export const SEARCH_CLIENTS = gql`
       phone
       entreprise
     }
+  }
+`;
+export const FILTER_FORMATEURS_WITH_EXP = gql`
+  query FilterFormateurWithExp($filterExp: filterFormateurInput!) {
+    FilterFormateurWithExp(filterExp: $filterExp) {
+      id
+      name
+      lastname
+      email
+      tarifConventionne
+    }
+  }
+`;
+
+export const FILTER_FORMATEURS_WITHOUT_EXP = gql`
+  query FilterFormateurWithoutExp($filterWithoutExp:FilterFormateursWithoutExpInput!){
+  FilterFormateurWithoutExp(filterWithoutExp:$filterWithoutExp){
+  id
+  name
+  lastname
+  email
+  competence
+  tarifConventionne}
   }
 `;
